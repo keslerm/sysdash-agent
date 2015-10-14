a56a615c7a46d548a4a70e067b1e8598330ab478
Morgan Kesler <keslerm@dasbiersec.com>
2015-10-14 16:34:25 -0400

    Adding cpu and testing some stat libraries

--- Gemfile
+++ Gemfile
@@ -4,4 +4,6 @@ source 'https://rubygems.org'
 gemspec
 
 gem 'rest-client'
-gem 'sysinfo'
+gem 'ohai'
+gem 'sys-cpu'
+gem 'usagewatch'
--- lib/server_console/agent.rb
+++ lib/server_console/agent.rb
@@ -1,21 +1,31 @@
-require "server_console/agent/version"
-require "json"
-require "rest-client"
-require "sysinfo"
+require 'server_console/agent/version'
+require 'json'
+require 'rest-client'
+require 'ohai'
+require 'pp'
+require 'usagewatch'
+
 
 # This is not gonna stay like this
 module ServerConsole
   module Agent
     def self.run(server, name, token)
 
-      sysinfo = SysInfo.new
+      system = Ohai::System.new
+      system.all_plugins
 
-      RestClient.post "http://#{server}/server/checkin", {
+      usw = Usagewatch
+
+      # Load average
+      cpu_usage = usw.uw_cpuused
+
+      RestClient.post "http://#{server}/server/heartbeat", {
                         'name' => name,
                         'token' => token,
 
                         # Stats
-                        'uptime' => sysinfo.uptime
+                        'uptime' => system.data['uptime'],
+                        'cpu_usage' => cpu_usage
 
                       }.to_json, :content_type => :json, :accept => :json
 
